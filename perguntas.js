db384a07e65966c6f713b88947cdbaa3fe2b429b
angelinasofia <160738220+angelinasofia@users.noreply.github.com>
2024-11-05 08:52:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Países',
+    'Qual país mais populoso do mundo?',
+    'Índia'
+)
+
+criaCartao(
+    'País',
+    'Qual país mais pobre do mundo?',
+    'Sudão do sul'
+)
+
+criaCartao(
+    'País',
+    'Qual país mais rico do mundo?',
+    'Noruega'
+)
+
+criaCartao(
+    'País',
+    'Qual país mais antigo do mundo?',
+    'Japão'
+)
+
+criaCartao(
+    'País',
+    'Qual país mais longe do Brasil?',
+    'China'
+)
+
+criaCartao(
+    'País',
+    'Qual país mais velho do mundo?',
+    'Etiópia'
+)
